4b17cc405b22f6025cdcf366c12c8a73ad79da36
nabeo <nabeo.dendai.master31@gmail.com>
2025-09-15 17:22:40 +0900

    day05c

--- kernel/graphics.cpp
+++ kernel/graphics.cpp
@@ -1,13 +1,13 @@
 #include "graphics.hpp"
 
-void RGBResv8BitPerColorPixelWriter:Write(int x, int y, const PixelColor& c) {
+void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
   auto p = PixelAt(x, y);
   p[0] = c.r;
   p[1] = c.g;
   p[2] = c.b;
 }
 
-void BGRResv8BitPerColorPixelWriter:Write(int x, int y, const PixelColor& c) override {
+void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
   auto p = PixelAt(x, y);
   p[0] = c.b;
   p[1] = c.g;
