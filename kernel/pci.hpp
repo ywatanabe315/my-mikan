ff74aa5e9ef04a584743cb6776fadcca6cf7dc10
nabeo <nabeo.dendai.master31@gmail.com>
2025-10-13 15:36:17 +0900

    day06c

--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -9,6 +9,21 @@ namespace pci {
   const uint16_t kConfigAddress = 0x0cf8;
   const uint16_t kConfigData = 0x0cfc;
 
+  struct ClassCode {
+    uint8_t base, sub, interface;
+
+    bool Match(uint8_t b) { return b == base; }
+    bool Match(uint8_t b, uint8_t s) { return Match(b) && s == sub; }
+    bool Match(uint8_t b, uint8_t s, uint8_t i) {
+      return Match(b, s) && i == interface;
+    }
+  };
+
+  struct Device {
+    uint8_t bus, device, function, header_type;
+    ClassCode class_code;
+  };
+
   void WriteAddress(uint32_t address);
   void WriteData(uint32_t value);
   uint32_t ReadData();
@@ -17,18 +32,28 @@ namespace pci {
   uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
   uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
 
-  uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+  ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+
+  inline uint16_t ReadVendorId(const Device& dev) {
+    return ReadVendorId(dev.bus, dev.device, dev.function);
+  }
+
+  uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr);
+
+  void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value);
 
   uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function);
 
   bool IsSingleFunctionDevice(uint8_t header_type);
 
-  struct Device {
-    uint8_t bus, device, function, header_type;
-  };
-
   inline std::array<Device, 32> devices;
   inline int num_devices;
 
   Error ScanAllBus();
+
+  constexpr uint8_t CalcBarAddress(unsigned int bar_index) {
+    return 0x10 + 4 * bar_index;
+  }
+
+  WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index);
 }
