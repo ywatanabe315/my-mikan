a925a18de7f667b45fde5687af9e7c6fe8e69f01
nabeo <nabeo.dendai.master31@gmail.com>
2025-10-19 18:42:43 +0900

    UEFIメモリマップ表示

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "frame_buffer_config.hpp"
+#include "memory_map.hpp"
 #include "graphics.hpp"
 #include "mouse.hpp"
 #include "font.hpp"
@@ -86,7 +87,8 @@ void InthandlerXHCI(InterruptFrame* frame) {
   NotifyEndOfInterrupt();
 }
 
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config,
+                           const MemoryMap& memory_map) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
       pixel_writer = new(pixel_writer_buf) RGBResv8BitPerColorPixelWriter{frame_buffer_config};
@@ -108,6 +110,26 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 
   printk("Welocome to MikanOS!\n");
   SetLogLevel(kWarn);
+
+  const std::array available_memory_types{
+    MemoryType::kEfiBootServicesCode,
+    MemoryType::kEfiBootServicesData,
+    MemoryType::kEfiConventionalMemory,
+  };
+
+  printk("memory_map: %p\n", &memory_map);
+  for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
+       iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
+       iter += memory_map.descriptor_size) {
+    auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
+    for (int i = 0; i < available_memory_types.size(); ++i) {
+      if (desc ->type == available_memory_types[i]) {
+        printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+          desc->type, desc->physical_start, desc->physical_start + desc->number_of_pages * 4096 - 1,
+          desc->number_of_pages, desc->attribute);
+      }
+    }
+  }
 
   mouse_cursor = new(mouse_cursor_buf) MouseCursor{
     pixel_writer, kDesktopBGColor, {300, 200}
