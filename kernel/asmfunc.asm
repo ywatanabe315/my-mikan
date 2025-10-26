973bc483c4dd59ca2d714a0e50a2858fe0c69b2a
nabeo <nabeo.dendai.master31@gmail.com>
2025-10-26 17:16:49 +0900

    各種データの移動

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -31,3 +31,56 @@ LoadIDT:
   mov rsp, rbp
   pop rbp
   ret
+
+global LoadGDT
+LoadGDT:
+  push rbp
+  mov rbp, rsp
+  sub rsp, 10
+  mov [rsp], di
+  mov [rsp + 2], rsi
+  lgdt [rsp]
+  mov rsp, rbp
+  pop rbp
+  ret
+
+global SetCSSS
+SetCSSS:
+  push rbp
+  mov rbp, rsp
+  mov ss, si
+  mov rax, .next
+  push rdi
+  push rax
+  o64 retf
+.next:
+  mov rsp, rbp
+  pop rbp
+  ret
+
+global SetDSAll
+SetDSAll:
+  mov ds, di
+  mov es, di
+  mov fs, di
+  mov gs, di
+  ret
+
+global SetCR3
+SetCR3:
+  mov cr3, rdi
+  ret
+
+extern kernel_main_stack
+extern KernelMainNewStack
+
+global KernelMain
+KernelMain:
+  ; OS用のスタックの先頭アドレスがkernel_main_stack。
+  ; スタックはメモリの末尾から頭に向かって伸びる？ので、最初のスタックポインタは、
+  ; スタックのメモリ領域（1MiB）の末尾に設定する必要がある。
+  mov rsp, kernel_main_stack + 1024 * 1024
+  call KernelMainNewStack
+.fin:
+  hlt
+  jmp .fin
