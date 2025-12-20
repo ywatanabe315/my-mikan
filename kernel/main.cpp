d8fd14d27c2cef8efe2ca6f56325051571e36451
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-20 17:35:54 +0900

    カウンタ表示

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -246,10 +246,8 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
   DrawMouseCursor(mouse_window->Writer(), {0, 0});
   mouse_position = {200, 200};
 
-  auto main_window = std::make_shared<Window>(160, 68, frame_buffer_config.pixel_format);
+  auto main_window = std::make_shared<Window>(160, 52, frame_buffer_config.pixel_format);
   DrawWindow(*main_window->Writer(), "Hello Window");
-  WriteString(*main_window->Writer(), {24, 28}, "Welcome to", {0, 0, 0});
-  WriteString(*main_window->Writer(), {24, 44}, " MikanOS world!", {0, 0, 0});
 
   FrameBuffer screen;
   if (auto err = screen.Initialize(frame_buffer_config)) {
@@ -276,10 +274,19 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
   Log(kDebug, "DrawLayers");
   layer_manager->Draw();
 
+  char str[128];
+  unsigned int count = 0;
+
   while (true) {
+    ++count;
+    sprintf(str, "%010u", count);
+    FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
+    WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
+    layer_manager->Draw();
+
     __asm__("cli");
     if (main_queue.Count() == 0) {
-      __asm__("sti\n\thlt");
+      __asm__("sti");
       continue;
     }
 
