8b0c3a1d315e430bd7b57bd25896d9826c8af72d
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-21 13:43:35 +0900

    バックバッファ導入

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -43,6 +43,7 @@ class LayerManager {
 
   private:
     FrameBuffer* screen_{nullptr};
+    mutable FrameBuffer back_buffer_{};
     std::vector<std::unique_ptr<Layer>> layers_{};
     std::vector<Layer*> layer_stack_{};
     unsigned int latest_id_{0};
