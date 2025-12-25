36a0ef7ebfe8048a220c49ea30e24548a3ebb2b8
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-25 23:15:53 +0900

    細かく時間を測る

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -2,6 +2,7 @@
 
 #include "asmfunc.h"
 #include "segment.hpp"
+#include "timer.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -30,7 +31,7 @@ namespace {
 
   __attribute__((interrupt))
   void IntHandlerLAPICTimer(InterruptFrame* frame) {
-    msg_queue->push_back(Message{Message::kInterruptLAPICTimer});
+    LAPICTimerOnInterrupt();
     NotifyEndOfInterrupt();
   }
 }
