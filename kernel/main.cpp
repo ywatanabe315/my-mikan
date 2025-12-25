4c968767d6d4448b9729606ac68dbfe5422e2f62
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-25 23:58:08 +0900

    タイムアウト通知

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -85,7 +85,10 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
   InitializeMouse();
   layer_manager->Draw({{0, 0}, ScreenSize()});
 
-  InitializeLAPICTimer();
+  InitializeLAPICTimer(*main_queue);
+
+  timer_manager->AddTimer(Timer(200, 2));
+  timer_manager->AddTimer(Timer(600, -1));
 
   char str[128];
   unsigned int count = 0;
@@ -114,8 +117,12 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     case Message::kInterruptXHCI:
       usb::xhci::ProcessEvents();
       break;
-    case Message::kInterruptLAPICTimer:
-      printk("Timer interrupt\n");
+    case Message::kTimerTimeout:
+      printk("Timer: timeout = %lu, value = %d\n",
+             msg.arg.timer.timeout, msg.arg.timer.value);
+      if (msg.arg.timer.value > 0) {
+        timer_manager->AddTimer(Timer(msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+      }
       break;
     default:
       Log(kError, "Unknown message type %d\n", msg.type);
