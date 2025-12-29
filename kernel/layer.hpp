eea31621c9404948d83993ea14ecff08dbedc6d5
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-29 17:49:34 +0900

    TaskBのWindow描画不具合解消

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -6,6 +6,7 @@
 
 #include "graphics.hpp"
 #include "window.hpp"
+#include "message.hpp"
 
 class Layer {
   public:
@@ -59,3 +60,4 @@ class LayerManager {
 extern LayerManager* layer_manager;
 
 void InitializeLayer();
+void ProcessLayerMessage(const Message& msg);
