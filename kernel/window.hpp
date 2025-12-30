3c2c0aee3f30437aac8d9cc3ebcd7f0929d996d1
nabeo <nabeo.dendai.master31@gmail.com>
2025-12-30 22:24:11 +0900

    ターミナルウィンドウ表示

--- kernel/window.hpp
+++ kernel/window.hpp
@@ -55,6 +55,8 @@ class ToplevelWindow : public Window {
   public:
     static constexpr Vector2D<int> kTopLeftMargin{4, 24};
     static constexpr Vector2D<int> kBottomRightMargin{4, 4};
+    static constexpr int kMarginX = kTopLeftMargin.x + kBottomRightMargin.x;
+    static constexpr int kMarginY = kTopLeftMargin.y + kBottomRightMargin.y;
 
     class InnerAreaWriter : public PixelWriter {
       public:
@@ -88,4 +90,5 @@ class ToplevelWindow : public Window {
 
 void DrawWindow(PixelWriter& writer, const char* title);
 void DrawTextbox(PixelWriter& writer, Vector2D<int> pos, Vector2D<int> size);
+void DrawTerminal(PixelWriter& writer, Vector2D<int> pos, Vector2D<int> size);
 void DrawWindowTitle(PixelWriter& writer, const char* title, bool active);
