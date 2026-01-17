3c1477bad59716a19ad0039adfa09b899f8d2461
nabeo <nabeo.dendai.master31@gmail.com>
2026-01-17 17:46:05 +0900

    catコマンド

--- kernel/fat.hpp
+++ kernel/fat.hpp
@@ -63,6 +63,7 @@ namespace fat {
   } __attribute__((packed));
 
   extern BPB* boot_volume_image;
+  extern unsigned long bytes_per_cluster;
   void Initialize(void* volume_image);
 
   uintptr_t GetClusterAddr(unsigned long cluster);
@@ -73,4 +74,12 @@ namespace fat {
   }
 
   void ReadName(const DirectoryEntry& entry, char* base, char* ext);
+
+  static const unsigned long kEndOfClusterchain = 0xffffffflu;
+
+  unsigned long NextCluster(unsigned long cluster);
+
+  DirectoryEntry* FindFile(const char* name, unsigned long directory_cluster = 0);
+
+  bool NameIsEqual(const DirectoryEntry& entry, const char* name);
 }
--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -31,6 +31,7 @@ class Terminal {
 
     void ExecuteLine();
     void Print(const char* s);
+    void Print(char c);
 
     std::deque<std::array<char, kLineMax>> cmd_history_{};
     int cmd_history_index_{-1};
