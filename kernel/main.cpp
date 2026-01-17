32b2c3ad03ce622221cb93a6f3d36855bb6db1f1
nabeo <nabeo.dendai.master31@gmail.com>
2026-01-17 16:31:20 +0900

    lsコマンド

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -32,6 +32,7 @@
 #include "keyboard.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
+#include "fat.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -130,6 +131,7 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
   InitializeMemoryManager(memory_map);
   InitializeInterrupt();
 
+  fat::Initialize(volume_image);
   InitializePCI();
 
   InitializeLayer();
@@ -155,22 +157,6 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
-
-  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
-  printk("Volume Image:\n");
-  for (int i = 0; i < 16; ++i) {
-    printk("%04x:", i * 16);
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk(" ");
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk("\n");
-  }
 
   char str[128];
   unsigned int count = 0;
