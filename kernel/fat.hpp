3833ed99d6280f62c1186f4427d3737ad2dfd153
nabeo <nabeo.dendai.master31@gmail.com>
2026-01-31 23:47:04 +0900

    階層Paging

--- kernel/fat.hpp
+++ kernel/fat.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <cstddef>
 
 namespace fat {
   struct BPB {
@@ -82,4 +83,6 @@ namespace fat {
   DirectoryEntry* FindFile(const char* name, unsigned long directory_cluster = 0);
 
   bool NameIsEqual(const DirectoryEntry& entry, const char* name);
+
+  size_t LoadFile(void* buf, size_t len, const DirectoryEntry& entry);
 }
